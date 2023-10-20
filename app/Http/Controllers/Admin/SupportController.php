80ef005719bdeabadb128b005b79b446d3af924b
Ernane Sabino <esabino@stefanini.com>
2023-10-20 14:21:20 -0300

    fix: fixing bugs

--- app/Http/Controllers/Admin/SupportController.php
+++ app/Http/Controllers/Admin/SupportController.php
@@ -13,7 +13,9 @@ use Illuminate\Http\Request;
 class SupportController extends Controller
 {
 
-    public function __construct(protected SupportService $service) {}
+    public function __construct(protected SupportService $service)
+    {
+    }
 
     public function index(Request $request)
     {
--- app/Repositories/SupportEloquentORM.php
+++ app/Repositories/SupportEloquentORM.php
@@ -1,10 +1,12 @@
 <?php
 
+namespace App\Repositories;
 
 use App\DTO\CreateSupportDTO;
 use App\DTO\UpdateSupportDTO;
 use App\Models\Support;
 use App\Repositories\SupportRepositoryInterface;
+use stdClass;
 
 class SupportEloquentORM implements SupportRepositoryInterface
 {
@@ -22,7 +24,7 @@ class SupportEloquentORM implements SupportRepositoryInterface
           $query->orWhere('body', 'like', "%{$filter}%");
         }
       })
-      ->all()
+      ->get()
       ->toArray();
   }
 
